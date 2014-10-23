83d0f12568e2e3b783188e945bc36e99a09da5fc
Mathias Karlgren <mathias.karlgren@gmail.com>
2014-10-23 21:49:07 +0200

    AngularJS routing to HTML5 mode and configured server rewrites

--- src/main/webapp/shop/app/js/app.js
+++ src/main/webapp/shop/app/js/app.js
@@ -11,38 +11,42 @@ var webshop = angular.module('WebShop', [
 ]);
 
 
-webshop.config(['$routeProvider',
-    function($routeProvider) {  // Injected object $routeProvider
+webshop.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {  // Injected object $routeProvider
+
         $routeProvider.
                 when('/home', {
-                    templateUrl: 'partials/home/home.html'
+                    templateUrl: 'shop/app/partials/home/home.html'
                     //controller: Not used
                 }).
                 when('/products', {
-                    templateUrl: 'partials/products/products.html',
+                    templateUrl: 'shop/app/partials/products/products.html',
                     controller: 'ProductListCtrl'
                 }).
                 when('/products/:id', {
-                    templateUrl: 'partials/products/product-detail.html',
+                    templateUrl: 'shop/app/partials/products/product-detail.html',
                     controller: 'ProductDetailCtrl'
                 }).
                 when('/product', {
-                    templateUrl: 'partials/products/product-new.html',
+                    templateUrl: 'shop/app/partials/products/product-new.html',
                     controller: 'ProductNewCtrl'
                 }).
                 when('/customers', {
-                    templateUrl: 'partials/customers/customers.html'
+                    templateUrl: 'shop/app/partials/customers/customers.html'
                     //controller: Not used
                 }).
                 when('/orders', {
-                    templateUrl: 'partials/orders/orders.html'
+                    templateUrl: 'shop/app/partials/orders/orders.html'
                     //controller: Not used
                 }).
                 when('/about', {
-                    templateUrl: 'partials/about/about.html'
+                    templateUrl: 'shop/app/partials/about/about.html'
                     //controller: Not used
                 }).
                 otherwise({
                     redirectTo: '/home'
                 });
+
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
     }]);
